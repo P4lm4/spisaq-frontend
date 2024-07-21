4465a5491ee176202b1979bc5e6b492805778c11
Igor Pavlović <pigorp@hotmail.com>
2024-07-21 15:36:21 +0200

    1. fix Content ( make input field and confirm button for changing name of the list) 2. fix BurgerMenu ( fixed Link component )

--- src/_root/pages/Content.jsx
+++ src/_root/pages/Content.jsx
@@ -4,6 +4,9 @@ function Content() {
 
   const [tasks, setTasks] = useState(["Eat breakfest", "Take a shower", "Feed a dog"]);
   const [newTask, setNewTask] = useState("");
+  const [changeInputType, setChangeInputType] = useState(false);
+  const [name, setName] = useState("Name of list");
+  const [newName, setNewName] = useState("");
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
@@ -40,24 +43,63 @@ function Content() {
     }
   }
 
+  function handleInputChangeName(event) {
+    setName(event.target.value);
+  }
+
+  function changeName() {
+
+    document.getElementById("name").type = "text";
+    document.getElementById("name").value = "";
+    document.getElementById("name").className = "bg-white text-black font-bold rounded-sm";
+    
+    setChangeInputType(true);
+  }
+
+  function confirmName() {
+
+    if(newName.trim() !== "") {
+      setName(n => [...n,newName]);
+      setNewName("");
+    }
+    else {
+      setName(name);
+    }
+    
+    document.getElementById("name").type = "button";
+    document.getElementById("name").value = {name};
+    document.getElementById("name").className = "bg-black text-white font-bold rounded-sm";
+
+    setChangeInputType(false);
+    
+  }
+
 
   return (
     <div className="flex flex-col items-center justify-center w-96 h-96 rounded-xl bg-black">
+      <div className='flex items-center justify-between'>
+        <input id="name" type="button" value={name} onClick={changeName} onChange={handleInputChangeName} className='bg-black text-white font-bold rounded-sm'></input>
+        {changeInputType && (<button className="bg-green-700 text-white rounded-sm m-2 px-1 font-bold" onClick={confirmName}>Edit</button>)}
+      </div>
 
-      <h1 className='text-white font-bold'>To-Do-List</h1>
 
-      <div>
+      <div className='flex items-center justify-between'>
         <input className='rounded-sm' type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange}/>
         <button className="bg-green-700 text-white rounded-sm m-2 px-1 font-bold" onClick={addTask}>Add</button>
       </div>
 
-      <ol>
+      <ol className='flex flex-col items-center justify-between'>
         {tasks.map((task, index) =>
-          <li key={index} className='text-white'>
-            <span className='text-white'>{task}</span> 
-            <button className='bg-red-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => deleteTask(index)}>Delete</button>
-            <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskUp(index)}>Up</button>
-            <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskDown(index)}>Down</button>
+          <li key={index} className='text-white flex flex-row items-center justify-center'>
+            <div className='flex justify-start'>
+              <span className='text-white'>{task}</span>
+            </div>
+            <div className=''>
+              <button className='bg-red-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => deleteTask(index)}>Delete</button>
+              <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskUp(index)}>Up</button>
+              <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskDown(index)}>Down</button>
+            </div>
+ 
           </li>)}
       </ol>
       

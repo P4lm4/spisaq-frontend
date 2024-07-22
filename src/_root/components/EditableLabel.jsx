9470dcd333e11fe8da1f02ced2da34a97650c109
Igor Pavlović <pigorp@hotmail.com>
2024-07-22 17:17:02 +0200

    1. dev EditableLabel ( make new component ) 2. fix Content ( clear code for editing name of list and make new component )

---
+++ src/_root/components/EditableLabel.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function EditableLabel() {
+  
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState('Click to edit');
+
+  const handleTextClick = () => {
+    setIsEditing(true);
+  };
+
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter') {
+      setIsEditing(false);
+    }
+  };
+
+
+return (
+  <div>
+    {isEditing ? (
+      <input
+        className="text-black font-bold"
+        type="text"
+        value={text}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+        onKeyDown={handleKeyDown}
+        autoFocus
+      />
+    ) : (
+      <span className="font-bold" onClick={handleTextClick}>{text}</span>
+    )}
+  </div>
+)
+
+}
+
+export default EditableLabel
--- src/_root/pages/Content.jsx
+++ src/_root/pages/Content.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
+import EditableLabel from '../components/EditableLabel';
 
 function Content() {
 
   const [tasks, setTasks] = useState(["Eat breakfest", "Take a shower", "Feed a dog"]);
   const [newTask, setNewTask] = useState("");
-  const [changeInputType, setChangeInputType] = useState(false);
-  const [name, setName] = useState("Name of list");
-  const [newName, setNewName] = useState("");
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
@@ -17,9 +15,7 @@ function Content() {
   if(newTask.trim() !== "") {
     setTasks(t => [...t,newTask]);
     setNewTask("");
-}
-
-
+  }
   }
 
   function deleteTask(index) {
@@ -43,65 +39,37 @@ function Content() {
     }
   }
 
-  function handleInputChangeName(event) {
-    setName(event.target.value);
-  }
-
-  function changeName() {
-
-    document.getElementById("name").type = "text";
-    document.getElementById("name").value = "";
-    document.getElementById("name").className = "bg-white text-black font-bold rounded-sm";
-    
-    setChangeInputType(true);
-  }
-
-  function confirmName() {
-
-    if(newName.trim() !== "") {
-      setName(n => [...n,newName]);
-      setNewName("");
-    }
-    else {
-      setName(name);
-    }
-    
-    document.getElementById("name").type = "button";
-    document.getElementById("name").value = {name};
-    document.getElementById("name").className = "bg-black text-white font-bold rounded-sm";
-
-    setChangeInputType(false);
-    
-  }
-
 
   return (
-    <div className="flex flex-col items-center justify-center w-96 h-96 rounded-xl bg-black">
-      <div className='flex items-center justify-between'>
-        <input id="name" type="button" value={name} onClick={changeName} onChange={handleInputChangeName} className='bg-black text-white font-bold rounded-sm'></input>
-        {changeInputType && (<button className="bg-green-700 text-white rounded-sm m-2 px-1 font-bold" onClick={confirmName}>Edit</button>)}
+    <div className="flex flex-col items-center justify-around w-96 h-96 rounded-xl bg-black">
+      
+      <div className='basis-1/3 flex flex-col items-center justify-center'>
+        <div className='text-white'>
+        <EditableLabel />
+        </div>
+
+        <div className=''>
+          <input className='rounded-sm' type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange}/>
+          <button className="bg-green-700 text-white rounded-sm m-2 px-1 font-bold" onClick={addTask}>Add</button>
+        </div>
       </div>
 
-
-      <div className='flex items-center justify-between'>
-        <input className='rounded-sm' type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange}/>
-        <button className="bg-green-700 text-white rounded-sm m-2 px-1 font-bold" onClick={addTask}>Add</button>
+      <div className='basis-2/3'>
+        <ol className=''>
+          {tasks.map((task, index) =>
+            <li key={index} className='text-white flex flex-row items-center justify-between'>
+              <div className=''>
+                <span className='text-white'>{task}</span>
+              </div>
+              <div className=''>
+                <button className='bg-red-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => deleteTask(index)}>Delete</button>
+                <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskUp(index)}>Up</button>
+                <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskDown(index)}>Down</button>
+              </div>
+            </li>)}
+        </ol>
       </div>
 
-      <ol className='flex flex-col items-center justify-between'>
-        {tasks.map((task, index) =>
-          <li key={index} className='text-white flex flex-row items-center justify-center'>
-            <div className='flex justify-start'>
-              <span className='text-white'>{task}</span>
-            </div>
-            <div className=''>
-              <button className='bg-red-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => deleteTask(index)}>Delete</button>
-              <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskUp(index)}>Up</button>
-              <button className='bg-blue-700 text-white rounded-sm m-1 px-1 font-bold' onClick={() => moveTaskDown(index)}>Down</button>
-            </div>
- 
-          </li>)}
-      </ol>
       
     </div>
   )

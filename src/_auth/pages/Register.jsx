4c3adca33d06642a4012b71f0e7c9fb75d2a5005
Igor Pavlović <pigorp@hotmail.com>
2024-08-01 23:25:52 +0200

    Chaned CSS Register component

--- src/_auth/pages/Register.jsx
+++ src/_auth/pages/Register.jsx
@@ -43,68 +43,79 @@ function Register() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center justify-center w-72 bg-black rounded-xl p-5 text-white gap-4">
-        <h1 className="text-2xl font-bold border-b-yellow-400 border-b-2 w-full text-center pb-1">
+      <div className="flex flex-col items-center justify-center w-72 bg-black rounded-xl p-5 text-white">
+        <h1 className="text-2xl font-bold border-b-yellow-300 border-b-2 w-full text-center mb-3 pb-4">
           Register
         </h1>
         <form
           onSubmit={handleSubmitRegister}
-          className="flex flex-col items-center justify-center"
+          className="flex flex-col items-center justify-center gap-2 pt-2"
         >
-          <label>
-            <p>Email</p>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              className="m-1 text-black p-1 opacity-90 mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          <div className="flex w-full flex-col gap-1">
+            <label>
+              <p className="text-xs text-yellow-300">Email</p>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                className="text-black opacity-90 block w-full py-2 px-2 bg-white border border-slate-300 rounded-md text-xs shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-yellow-400 focus:ring-1 focus:ring-yellow-400"
+                placeholder="Email"
+                type="email"
+              ></input>
+            </label>
+          </div>
+          <div className="flex w-full flex-col gap-1">
+            <label>
+              <p className="text-xs text-yellow-300">Username</p>
+              <input
+                onChange={(e) => setUsername(e.target.value)}
+                className="text-black opacity-90 block w-full py-2 px-2 bg-white border border-slate-300 rounded-md text-xs shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-yellow-400 focus:ring-1 focus:ring-yellow-400"
+                placeholder="Username"
+                type="text"
+              ></input>
+            </label>
+          </div>
+          <div className="flex w-full flex-col gap-1">
+            <label>
+              <p className="text-xs text-yellow-300">Password</p>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                className="text-black opacity-90 block w-full py-2 px-2 bg-white border border-slate-300 rounded-md text-xs shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-yellow-400 focus:ring-1 focus:ring-yellow-400"
+                placeholder="Password"
+                type="password"
+              ></input>
+            </label>
+          </div>
+          <div className="flex w-full flex-col gap-1">
+            <label>
+              <p className="text-xs text-yellow-300">Confirm password</p>
+              <input
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="m-1 text-black p-1 opacity-90 mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
           focus:outline-none focus:border-yellow-400 focus:ring-1 focus:ring-yellow-400"
-              placeholder="Email"
-              type="email"
-            ></input>
-          </label>
-          <label>
-            <p>Username</p>
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              className="m-1 text-black p-1 opacity-90 mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-          focus:outline-none focus:border-yellow-400 focus:ring-1 focus:ring-yellow-400"
-              placeholder="Username"
-              type="text"
-            ></input>
-          </label>
-          <label>
-            <p>Password</p>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              className="m-1 text-black p-1 opacity-90 mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-          focus:outline-none focus:border-yellow-400 focus:ring-1 focus:ring-yellow-400"
-              placeholder="Password"
-              type="password"
-            ></input>
-          </label>
-          <label>
-            <p>Confirm password</p>
-            <input
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="m-1 text-black p-1 opacity-90 mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-          focus:outline-none focus:border-yellow-400 focus:ring-1 focus:ring-yellow-400"
-              placeholder="Confirm password"
-              type="password"
-            ></input>
-          </label>
-
-          <button
-            type="submit"
-            className="bg-yellow-700 rounded-xl mt-3 px-8 p-1 font-bold text-xl"
-          >
-            Sign up
-          </button>
+                placeholder="Confirm password"
+                type="password"
+              ></input>
+            </label>
+          </div>
+          <div className="flex flex-col items-center justify-center w-full">
+            <button
+              type="submit"
+              className="bg-yellow-300 rounded-xl my-3 py-2 w-full border-black font-bold text-black text-md"
+            >
+              Sign up
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center w-full">
+            <button
+              className="bg-yellow-300 rounded-xl my-3 py-2 w-full border-black font-bold text-black text-md"
+              onClick={() => navigate("/auth")}
+            >
+              Back
+            </button>
+          </div>
         </form>
-        <button
-          className="bg-yellow-700 rounded-xl px-4 p-1 border-black font-bold text-xl"
-          onClick={() => navigate("/auth")}
-        >
-          Back
-        </button>
       </div>
     </div>
   );
